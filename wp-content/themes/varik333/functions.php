23e4da210eaa6f00d9a483cdee13a9e163e18cb6
satik88 <86455351+satik88@users.noreply.github.com>
2021-07-12 23:26:19 +0300

    12

--- wp-content/themes/varik333/functions.php
+++ wp-content/themes/varik333/functions.php
@@ -178,3 +178,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+add_theme_support( 'woocommerce' );
+}
