b8927355b07c588c82201d1802cc12207994d2e8
satik88 <86455351+satik88@users.noreply.github.com>
2021-07-12 22:38:36 +0300

    8

---
+++ wp-content/plugins/anti-func/anti-func.php
@@ -0,0 +1,12 @@
+<?php
+/* ====================================
+ * Plugin Name: Anti-Func
+ * Description: Плагин для добавления сторонних кодов, чтобы не залезать в файл functions.php и не ронять сайт при не правильном коде. 
+ * Plugin URI: https://www.youtube.com/watch?v=D5kbnrRSbQo
+ * Author: Artem Abramovich
+ * Author URI: http://artabr.ru/
+ * Version: 1.0
+ * ==================================== */
+
+
+
