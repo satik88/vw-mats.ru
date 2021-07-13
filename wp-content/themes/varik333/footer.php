c8e6256d669f73e7b1824611c036f99a476c57b8
satik88 <86455351+satik88@users.noreply.github.com>
2021-07-13 18:04:39 +0300

    27

--- wp-content/themes/varik333/footer.php
+++ wp-content/themes/varik333/footer.php
@@ -11,24 +11,92 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'varik333' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'varik333' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'varik333' ), 'varik333', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-
+<footer class="footer">
+    <div class="container">
+      <div class="footer__top">
+        <div class="footer__top-inner">
+          <div class="footer__top-item footer__top-newsletter">
+            <h6 class="footer__top-title footer__top__title">
+              Подпишитесь на нашу рассылку
+              и узнавайте о акция быстрее
+            </h6>
+            <form action="" class="footer-form">
+              <input class="footer-form__input" type="text" placeholder="Введите ваш e-mail:">
+              <button class="footer-form__button" type="submit">Отправить</button>
+            </form>
+          </div>
+          <div class="footer__top-item">
+            <h6 class="footer__top-title footer__section-title">
+              Информация
+            </h6>
+            <ul class="footer-list">
+              <li class="footer-list__item">
+                <a href="#">О компании</a>
+              </li>
+              <li class="footer-list__item">
+                <a href="#">Контакты</a>
+              </li>
+              <li class="footer-list__item">
+                <a href="#">Акции</a>
+              </li>
+              <li class="footer-list__item">
+                <a href="#">Магазины</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer__top-item">
+            <h6 class="footer__top-title footer__section-title">
+              Интернет-магазин
+            </h6>
+            <ul class="footer-list">
+              <li class="footer-list__item">
+                <a href="#">Доставка и самовывоз</a>
+              </li>
+              <li class="footer-list__item">
+                <a href="#">Оплата</a>
+              </li>
+              <li class="footer-list__item">
+                <a href="#">Возврат-обмен</a>
+              </li>
+              <li class="footer-list__item">
+                <a href="#">Новости</a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer__top-item footer__top-social">
+            <ul class="social-list">
+              <li class="social-list__item">
+                <a href="#">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.svg" alt="">
+                </a>
+              </li>
+              <li class="social-list__item">
+                <a href="#">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/vk.svg" alt="">
+                </a>
+              </li>
+              <li class="social-list__item">
+                <a href="#">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.svg" alt="">
+                </a>
+              </li>
+              <li class="social-list__item">
+                <a href="#">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.svg" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer__bottom">
+        <a class="footer__bottom-link" href="#">Договор оферты</a>
+        <a class="footer__bottom-link" href="#">Политика обработки персональных данных</a>
+      </div>
+    </div>
+  </footer>
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
+
