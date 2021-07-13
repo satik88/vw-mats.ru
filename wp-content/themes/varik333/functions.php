ad762daf6cd9d428271325f34093f1ad09a33532
satik88 <86455351+satik88@users.noreply.github.com>
2021-07-13 15:14:10 +0300

    20 vw-mats.ru-20210713-121248-hi0cyy.wpress

--- wp-content/themes/varik333/functions.php
+++ wp-content/themes/varik333/functions.php
@@ -141,9 +141,16 @@ add_action( 'widgets_init', 'varik333_widgets_init' );
  */
 function varik333_scripts() {
 	wp_enqueue_style( 'varik333-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style('varik333-normalize', get_template_directory_uri() . '/assets/css/normalize.css', array(), _S_VERSION);
+	wp_enqueue_style('varik333-main-style', get_template_directory_uri() . '/assets/css/style.css', array(), _S_VERSION);
+	wp_enqueue_style('varik333-slick', get_template_directory_uri() . '/assets/css/slick.css', array(), _S_VERSION);
+	wp_enqueue_style('varik333-fonts', get_template_directory_uri() . '/assets/css/fonts.css', array(), _S_VERSION);
+		
 	wp_style_add_data( 'varik333-style', 'rtl', 'replace' );
-
+	
 	wp_enqueue_script( 'varik333-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('varik333-slick.min', get_template_directory_uri() . '/assets/js/slick.min.js', array(), _S_VERSION, true);
+	wp_enqueue_script('varik333-main', get_template_directory_uri() . '/assets/js/main.js', array(), _S_VERSION, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -178,6 +185,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// Подключаем WooCommerce
 add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
 add_theme_support( 'woocommerce' );
